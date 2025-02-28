c0e4b5cefee81453f23efd39595560760b151c0d
Tet Nay Lin <tetnaylin168@gmail.com>
2025-03-01 04:48:43 +1100

    Add SearchBar to HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 // import LeadGrid from "../components/LeadGrid";
 // import FreeRoomsLogo from '../assets/freeRoomsLogo.png';
 import Header from '../components/Header'
+import SearchBar from '../components/SearchBar';
 
 function HomePage() {
     return (
         <div >
             <Header/>
+            <SearchBar/>
             {/* <h1>Home Page :3</h1> */}
         </div>
     );
